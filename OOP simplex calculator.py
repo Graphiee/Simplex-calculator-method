2bf23bbc45cd9024e0c089671574ebccf762ba61
Dominik Miśkiewicz <dominikmiskiewicz@gmail.com>
2019-11-23 17:29:15 +0100

    Things to do: 1) Add base and setting up new RHS 2) After 1st iteration something goes wrong- fix it

--- OOP simplex calculator.py
+++ OOP simplex calculator.py
@@ -2,44 +2,84 @@ import numpy as np
 
 class Simplex:
 
-    def __init__(self, objective, constraints, inequality_dir ,maximalize=True):
+    def __init__(self, objective, lhs, rhs, inequality_dir ,maximalize=True):
         self.objective = objective
-        self.constraints = constraints
-        self.maximalize = maximalize
+        self.lhs = lhs
         self.inequality_dir = inequality_dir
+        self.rhs = rhs
+        self.maximalize = maximalize
 
     def fit(self, num_iter=100):
 
         obj = np.array(self.objective.copy())
-        con = np.array(self.constraints.copy())
+        lhs = np.array(self.lhs.copy())
+        rhs = np.array(self.rhs.copy())
+
         inequalities = np.array(self.inequality_dir)
         slacks = sum((inequalities == '>=') | (inequalities == '<='))
         artificial = sum((inequalities == '=') | (inequalities == '>='))
 
         obj = np.append(obj, [0 for i in range(slacks)])
-        obj = np.append(obj, [-1e64 for i in range(artificial)])
+        obj = np.append(obj, [-1e64 if self.maximalize==True else 1e64 for i in range(artificial)])
+        #Slacks
+        if slacks > 0 :
+            zeros = np.zeros((len(lhs) * slacks))
+            rows = np.where((inequalities == '>=') | (inequalities == '<='))[0]
+            columns = np.array(range(slacks))
+            np.put(zeros, slacks * rows + columns, np.ones(slacks))
+            zeros = np.where(np.tile(inequalities, slacks) == '>=', -zeros, zeros)
+            zeros = zeros.reshape((len(lhs), slacks))
+            lhs = np.concatenate((lhs, zeros), axis=1)
 
-        constraints_m = {}
-        for i in range(num_iter):
-            for constraint in range(len(con)):
-                if len(con) == to_append_slack:
-                    break
-                to_append_slack = np.zeros(slacks)
-                to_append_art = np.zeros(artificial)
-                if inequalities[constraint] == '<=':
-                    to_append_slack[constraint] = 1
-                    constraints_m['Constraint_' + str(constraint)] = np.append(con[constraint], to_append_slack)
-                elif inequalities[constraint] == '>=':
-                    to_append_slack[constraint] = -1
-                    constraints_m['Constraint_' + str(constraint)] = np.append(con[constraint], to_append_slack)
+        #Artificials
+        if artificial > 0:
+            zeros = np.zeros((len(lhs) * artificial))
+            rows = np.where((inequalities == '>=') | (inequalities == '='))[0]
+            columns = np.array(range(artificial))
+            np.put(zeros, artificial*rows+columns, np.ones(artificial))
+            zeros = zeros.reshape((len(lhs), artificial))
+            lhs = np.concatenate((lhs, zeros), axis=1)
 
-            # for constraint in range(len(con)):
-            #     if inequalities[constraint] == '=':
-            #         constraints_m['Constraint_' + str(constraint)] = np.append(con[constraint], to_append_slack)
+        for iteration in range(num_iter):
+            print(iteration)
+            #z_j - obj
+            base_indexes = np.where((np.sum(lhs, axis=0) == 1) & (np.sum(lhs == 0, axis=0) == len(lhs)-1))[0]
+            base_values = obj[base_indexes]
+            z_j = np.dot(base_values.reshape(1,-1), lhs)
+            obj_zj = z_j - obj
+            obj_zj = obj_zj.flatten()
+
+
+            #Pivot element
+            if self.maximalize:
+                if any(obj_zj < 0):
+                    obj_zj_index = int(np.where(obj_zj == min(obj_zj))[0])
+                    vector_ratio = rhs/lhs[:, obj_zj_index][0]
+                else:
+                    print('Optimal solution reached after {} iterations.'.format(iteration))
+            elif not self.maximalize:
+                if any(obj_zj > 0):
+                    obj_zj_index = int(np.where(obj_zj == max(obj_zj))[0])
+                    vector_ratio = rhs/lhs[:, obj_zj_index][0]
+                else:
+                    print('Optimal solution reached after {} iterations.'.format(iteration))
+
+            min_ratio_index = int(np.where(vector_ratio == min(vector_ratio))[0])
+            pivot = lhs[min_ratio_index, obj_zj_index]
+
+            new_row = lhs[min_ratio_index] / pivot
+            lhs = lhs - lhs[:, obj_zj_index].reshape(-1,1) * new_row
+            lhs[min_ratio_index,:] = new_row
+        return lhs
+
 
 
 first = Simplex(objective = [30, 20],
-                constraints = [[2, 1, 1000],
-                               [3, 3, 2400],
-                               [1.5, 600]],
-                inequality_dir = ['>=', '=', '>='])
+                lhs = [[2, 1],
+                       [3, 3],
+                       [1.5, 0]],
+                rhs = [1000,2400,600] ,
+                inequality_dir = ['<=', '<=', '<='],
+                maximalize=True)
+
+print(first.fit(num_iter=2))
